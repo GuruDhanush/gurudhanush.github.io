fc08c225097302bf9c0a4f3ff784c8611ca35eea
GuruDhanush <gurudhanush@outlook.com>
2020-04-25 13:19:38 +0530

    added darkmode and rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -73,8 +72,61 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-use-dark-mode`
+    ,{
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Guru Blog RSS feed",
+            match: "^/blog/"
+          },
+        ],
+      },
+    },
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import useDarkMode from 'use-dark-mode';
+import RSS from '../../static/feed-icon.png';
+
 
 import { rhythm, scale } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
+  const darkMode = useDarkMode(false);
 
   if (location.pathname === rootPath) {
     header = (
       <h1
         style={{
-          ...scale(1.5),
+          ...scale(1.2),
           marginBottom: rhythm(1.5),
           marginTop: 0,
         }}
@@ -58,10 +62,36 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
-      <header>{header}</header>
+      <div style={{
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `baseline`
+      }} >
+        <header>{header}</header>
+        <div>
+          <button type="button" style={{
+            border: `none`,
+            background: `transparent`,
+            outline: `none`
+          }} onClick={darkMode.toggle}>
+            {
+              darkMode.value ? ("☀") : ("☾")
+            }
+          </button>
+        </div>
+      </div>
+
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}
+      <footer  style={{
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `center`
+      }} >
+        <div>© {new Date().getFullYear()} </div>
+        <a  href="/rss.xml" style={{boxShadow: `none`, height:`14px`}} target="_blank">
+          <img height="14" width="14" src={RSS} alt="rss-feed"></img>
+        </a>
+
       </footer>
     </div>
   )
